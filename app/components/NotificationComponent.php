db2729cc23897c54cea04ca47a99ddfe66d9fbac
cuirassier1914 <s.pachkov@mail.ru>
2019-03-10 19:01:06 +0300

    Отправка писем с уведомлениями

---
+++ app/components/NotificationComponent.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sergio
+ * Date: 10.03.2019
+ * Time: 17:15
+ */
+
+namespace app\components;
+
+
+use app\models\Activity;
+use yii\base\Component;
+use yii\mail\MailerInterface;
+
+class NotificationComponent extends Component
+{
+    /** @var MailerInterface */
+
+    public $mailer;
+
+    /**
+     * @param $activities Activity[]
+     * @return \Generator
+     */
+
+    public function sendTodayNotification($activities)
+    {
+        foreach ($activities as $activity) {
+            $result = $this->mailer->compose('notification', ['model' => $activity])
+                ->setFrom('kirasirTest@yandex.ru')
+                ->setTo([$activity->user->email])
+                ->setSubject('События на сегодня')
+                ->setReplyTo('kirasirTest@yandex.ru')
+                ->attach(\Yii::getAlias('@app/web/images/72341551458286.jpg'))
+                ->setCharset('utf8')
+                ->send();
+            yield ['result' => $result, 'email' => $activity->user->email];
+        }
+    }
+}
